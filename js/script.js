b38f1130c82bc7654d9310a8e54f2ae8c020232d
Caroline Silva <contatocarolinebs@gmail.com>
2018-12-10 21:05:57 -0200

    Modificado validação de campos

--- js/script.js
+++ js/script.js
@@ -2,12 +2,34 @@ jQuery(document).ready(function() {
 	jQuery('.tabs .tab-links a').on('click', function(e) {
 		var currentAttrValue = jQuery(this).attr('href');
 
-		// Show/Hide Tabs
-		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
-
-		// Change/remove current tab to active
-		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
 		e.preventDefault();
 	});
 });
+
+const input_phone = document.getElementById("phone")
+
+input_phone.addEventListener("focus" , function(event) {
+    input_phone.value = "(__)_____-____"
+    setTimeout(function() {
+        input_phone.setSelectionRange(0, 0)
+    }, 1)
+})
+
+input_phone.addEventListener("blur" , function() {
+    this.value = ""
+})
+
+input_phone.addEventListener("keydown", function(event) {
+    event.preventDefault()
+    if("0123456789".indexOf(event.key) !== -1
+        && this.value.indexOf("_") !== -1) {
+            this.value = this.value.replace(/_/, event.key)
+            const next_index = this.value.indexOf("_")
+            this.setSelectionRange(next_index, next_index)
+    } else if (event.key === "Backspace") {
+        this.value = this.value.replace(/(\d$)|(\d(?=\D+$))/, "_")
+        const next_index = this.value.indexOf("_")
+        this.setSelectionRange(next_index, next_index)
+    }
+})
